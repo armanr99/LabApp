b639e8a8bbf64650b44e6f8cbdccd4978d1c03b0
armanr99 <arman.rostami.999@gmail.com>
2020-07-17 20:06:00 +0430

    Add Address class

---
+++ src/main/java/Models/User/Address.java
@@ -0,0 +1,13 @@
+package main.java.Models.User;
+
+public class Address {
+    private String city;
+    private String district;
+    private String fullAddress;
+
+    public Address(String city, String district, String fullAddress) {
+        this.city = city;
+        this.district = district;
+        this.fullAddress = fullAddress;
+    }
+}
