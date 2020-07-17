83a26c143da713e1e88a5af2962c55ca81eca539
armanr99 <arman.rostami.999@gmail.com>
2020-07-17 19:58:57 +0430

    Add User class

---
+++ src/main/java/Models/User/User.java
@@ -0,0 +1,13 @@
+package main.java.Models.User;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
