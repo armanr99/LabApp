6132692ceece10b903af7c88519267d937616b62
armanr99 <arman.rostami.999@gmail.com>
2020-07-20 00:25:35 +0430

    Add lab initialize

--- src/main/java/models/LabApp/ObjectsInitializer.java
+++ src/main/java/models/LabApp/ObjectsInitializer.java
@@ -1,6 +1,7 @@
 package main.java.models.LabApp;
 
 import main.java.models.General.Address;
+import main.java.models.Lab.Lab;
 import main.java.models.Storage.Storage;
 import main.java.models.User.Patient;
 
@@ -9,6 +10,7 @@ import java.io.InvalidObjectException;
 public class ObjectsInitializer {
     public void initialize() throws InvalidObjectException {
         addPatients();
+        addLabs();
     }
 
     private void addPatients() throws InvalidObjectException {
@@ -16,5 +18,15 @@ public class ObjectsInitializer {
         Patient patient = new Patient(0, "Arman Rostami", "arman.rostami.999@gmail.com", "123",
                 "123", patientAddress);
         Storage.getInstance().getPatientRepository().insert(patient);
+    }
+
+    private void addLabs() throws InvalidObjectException {
+        Address labAddress = new Address("Tehran", "Somewhere", "Somewhere in Tehran");
+
+        Lab lab1 = new Lab(0, "Test Lab 1", labAddress);
+        Storage.getInstance().getLabRepository().insert(lab1);
+
+        Lab lab2 = new Lab(1, "Test Lab 1", labAddress);
+        Storage.getInstance().getLabRepository().insert(lab2);
     }
 }
